05521a48c40c69b532473cd543d364661e5efffe
Donovan Mueller <zotobi@gmail.com>
2018-01-31 08:50:50 -0700

    Setup PostCSS processing.

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+	parser: require('postcss-scss'), // For inline comment support in .pcss files.
+	plugins: [
+		require('postcss-import'),
+		require('postcss-property-lookup'),
+		require('postcss-nested'),
+		require('postcss-simple-vars'),
+		require('postcss-color-function'),
+		require('autoprefixer'),
+	]
+}
